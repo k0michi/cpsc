4f4845b853f6eb5399d174979c115e9e1a026d29
k0michi <k0michi@koyomi.co>
2026-08-31 22:04:17 +0900

    test: add includes

--- snippets/test-support.hh
+++ snippets/test-support.hh
@@ -4,14 +4,20 @@
 #include <algorithm>
 #include <bit>
 #include <concepts>
+#include <cassert>
+#include <deque>
 #include <functional>
+#include <initializer_list>
 #include <limits>
 #include <map>
 #include <numeric>
+#include <optional>
 #include <queue>
+#include <ranges>
 #include <set>
 #include <string>
 #include <type_traits>
+#include <tuple>
 #include <utility>
 #include <vector>
 
